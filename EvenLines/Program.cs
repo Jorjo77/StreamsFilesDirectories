57ce57f9c570b13bbd1b55a2bf44b06a2196dd8f
Georgi Kolev <tuvkontrol55@gmail.com>
2020-10-05 16:45:31 +0300

    Update tasks

---
+++ EvenLines/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                string line = reader.ReadLine();
+                int lineCounter = 0;
+                while (line!=null)
+                {
+
+                    if (lineCounter%2==0)
+                    {
+                        string pattern = @"[.,!?-]";
+                        Regex regex = new Regex(pattern);
+                        line = regex.Replace(line, "@");
+                        var reversedLine = line.Split().ToArray().Reverse();
+                        Console.WriteLine(string.Join(" ", reversedLine));
+
+                    }
+                    lineCounter++;
+                    line = reader.ReadLine();
+                }
+            }
+            //ако искаме да го запишем във файл:
+            //using (StreamReader reader = new StreamReader("../../../text.txt"))
+            //{
+            //    using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+            //    {
+            //        string line = reader.ReadLine();
+            //        int lineCounter = 0;
+            //        while (line != null)
+            //        {
+
+            //            if (lineCounter % 2 == 0)
+            //            {
+            //                string pattern = @"[.,!?-]";
+            //                Regex regex = new Regex(pattern);
+            //                line = regex.Replace(line, "@");
+            //                var reversedLine = line.Split().ToArray().Reverse();
+            //                writer.WriteLine(string.Join(" ", reversedLine));
+
+            //            }
+            //            lineCounter++;
+            //            line = reader.ReadLine();
+            //        }
+            //    }
+            //}
+        }
+    }
+}
---
+++ LineNumbers2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LineNumbers2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../text.txt");
+            string[] result = new string[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                int letterCount = lettersCounter(line);
+                int punctuationsCount = punctuationsCounter(line);
+
+                result[i] = ($"Line {i + 1}: {line} ({letterCount})({punctuationsCount})");
+                File.WriteAllLines("../../../output.txt", result);
+            }
+
+        }
+        static int lettersCounter(string line)
+        {
+            int counter = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                char currSymbol = line[i];
+                if (Char.IsLetter(currSymbol))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        static int punctuationsCounter(string line)
+        {
+            int counter = 0;
+            char[] punctuationsSymbols = new char[] { '-', '.', ',', '\'', ':', ';', '!', '?' };
+            for (int i = 0; i < line.Length; i++)
+            {
+                char currSymbol = line[i];
+                if (punctuationsSymbols.Contains(currSymbol))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
